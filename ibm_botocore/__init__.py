69adb1cb076ab784a4cd70f90ad5efee836b16b9
Bill Huber <wbhuber@us.ibm.com>
2018-10-11 19:02:30 -0500

    Release 2.3.1

--- ibm_botocore/__init__.py
+++ ibm_botocore/__init__.py
@@ -28,7 +28,7 @@ import logging
 
 
 __author__ = 'IBM'
-__version__ = '2.3.1.dev1'
+__version__ = '2.3.1'
 
 
 class NullHandler(logging.Handler):
--- setup.py
+++ setup.py
@@ -1,28 +1,18 @@
 #!/usr/bin/env python
 import ibm_botocore
 import sys
-
 from setuptools import setup, find_packages
+
+# IbmCos sdk python version check
+_valid  =  sys.version_info[:2] == (2, 7) or sys.version_info >= (3,4)
+if not _valid:
+    sys.exit("Sorry, IBM COS SDK only supports versions 2.7, 3.4, 3.5, 3.6, 3.7 of python.")
 
 
 requires = ['jmespath>=0.7.1,<1.0.0',
-            'python-dateutil>=2.1,<3.0.0',
             'docutils>=0.10',
-            'requests>=2.12.0,<=2.18.4']
-
-
-if sys.version_info[:2] == (2, 6):
-    # For python2.6 we have a few other dependencies.
-    # First we need an ordered dictionary so we use the
-    # 2.6 backport.
-    requires.append('ordereddict==1.1')
-    # Then we need simplejson.  This is because we need
-    # a json version that allows us to specify we want to
-    # use an ordereddict instead of a normal dict for the
-    # JSON objects.  The 2.7 json module has this.  For 2.6
-    # we need simplejson.
-    requires.append('simplejson==3.3.0')
-
+            'urllib3>=1.20,<1.24',
+            'python-dateutil>=2.1,<3.0.0']
 
 setup(
     name='ibm-cos-sdk-core',
@@ -37,12 +27,6 @@ setup(
                   'ibm_botocore.vendored.requests': ['*.pem']},
     include_package_data=True,
     install_requires=requires,
-    extras_require={
-        ':python_version=="2.6"': [
-            'ordereddict==1.1',
-            'simplejson==3.3.0',
-        ]
-    },
     license="Apache License 2.0",
     classifiers=(
         'Development Status :: 5 - Production/Stable',
@@ -52,12 +36,11 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ),
 )
