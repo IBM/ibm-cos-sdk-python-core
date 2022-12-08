66446e54de95aa3796fbe98a3c350dda22e04f3f
Alok Mitra <Alok.Mitra1@ibm.com>
2022-12-08 12:40:29 +0530

    Release 2.12.1

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ import ibm_botocore
 requires = [
     'jmespath>=0.10.0,<1.0.0',
     'python-dateutil>=2.8.2,<3.0.0',
-    'requests>=2.27.1,<3.0',
-    'urllib3>=1.26.9,<1.27',
+    'requests>=2.28.1,<3.0',
+    'urllib3>=1.26.13,<1.27',
 ]
 
 setup(
